f3d3c0cfbf279f3066d744349c93368484e61f42
EbukVick <55829039+Chukwuebuka2@users.noreply.github.com>
2022-03-11 00:59:40 +0100

    This is very sweet

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
  */
-
-
 
 int main(void)
 
 {
 
 	char acharacter;
-
 	int ainteger;
-
 	long along;
-
 	long long alonglong;
-
 	float afloat;
 	
 	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(acharacter));
