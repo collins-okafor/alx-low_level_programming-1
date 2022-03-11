19c1193cd6569694c9ad3258392a805ba48cad22
EbukVick <55829039+Chukwuebuka2@users.noreply.github.com>
2022-03-11 01:39:24 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char acharacter;
+	char b;
 	int ainteger;
 	long along;
 	long long a;
 	float afloat;
-	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(acharacter));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(b)); 
 	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(a));
