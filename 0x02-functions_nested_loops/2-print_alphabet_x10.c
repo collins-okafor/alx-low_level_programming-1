1bd2dcb5f6ded9f7a98610cee3201dc90561aa15
root <nwokporochukwuebuka@gmail.com>
2022-03-15 13:51:52 -0700

    Omo

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase and ten times
+ * 
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		int i = 97;
+
+		while (i < 123)
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar("\n");
+		x++;
+	}
+}
