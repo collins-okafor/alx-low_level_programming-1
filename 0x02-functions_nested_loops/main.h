2c8c5c389ba4f76becead54916f2da3c50415e9e
EbukVick <55829039+Chukwuebuka2@users.noreply.github.com>
2022-03-15 16:14:26 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 void print_alphabet(void);
+/*
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,5 +20,6 @@ void pr_3_digit(int charcode, int fmt);
 void print_times_table(int n);
 void print_spaces(int n);
 void print_zero(void);
+*/
 
 #endif
