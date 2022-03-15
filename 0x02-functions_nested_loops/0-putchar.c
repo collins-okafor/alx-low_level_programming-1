810ddf6cff5b0712ab1600ed9a16abaf3271a448
root <nwokporochukwuebuka@gmail.com>
2022-03-15 08:25:25 -0700

    Omo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,18 @@
  * Description: A program that prints _putchar and new line
  *
  * Return: returns 0
- */
+ **/
+
+int main(void)
+{
+	char text[8] = "_putchar";
 	int i = 0;
-	char p[] = "_putchar";
 
 	for (i = 0; i < 8; i++)
 		{
-		_putchar(p[i]);
+		_putchar(text[i]);
 		}
 	_putchar("\n");
+
 	return (0);
+}
