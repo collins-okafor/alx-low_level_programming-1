0a7ace55ab528329d89feb1ac4895914ba325bb6
root <nwokporochukwuebuka@gmail.com>
2022-03-17 07:21:32 -0700

    hbhsdh

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all numbers except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+	for (i = '0'; i <= '9')
+	{
+		if (i != '2' || i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
