0cbadb7e0a37cb187a12e69ac80937e89e163369
root <nwokporochukwuebuka@gmail.com>
2022-03-30 12:02:04 -0700

    Omo

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: input variable
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+	_putchar('\n');
+}
