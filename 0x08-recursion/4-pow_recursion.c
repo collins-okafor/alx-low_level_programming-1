a3232199cf34e91f1c5b535d50addeb309363c79
root <nwokporochukwuebuka@gmail.com>
2022-03-30 14:32:38 -0700

    xghd

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - finds the exponent of two numbers using recursion
+ * @x: varable x
+ * @y: variable y
+ * Return: the value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
